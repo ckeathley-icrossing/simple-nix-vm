22f5bc40c0d278e7b3cbc93ba9bddad0eae1bf50
ckeathley-icrossing <131910739+ckeathley-icrossing@users.noreply.github.com>
2023-08-22 14:46:30 -0500

    Update vm.nix

--- vm.nix
+++ vm.nix
@@ -5,29 +5,11 @@
 # Then connect with ssh -p 2222 guest@localhost
 { lib, config, pkgs, ... }:
 {
-  # Internationalisation options
+  system.stateVersion = "23.11";
+
   i18n.defaultLocale = "en_US.UTF-8";
-  console.keyMap = "fr";
+  console.keyMap = "us";
 
-  # Options for the screen
-  virtualisation.vmVariant = {
-    virtualisation.resolution = {
-      x = 1280;
-      y = 1024;
-    };
-    virtualisation.qemu.options = [
-      # Better display option
-      "-vga virtio"
-      "-display gtk,zoom-to-fit=false"
-      # Enable copy/paste
-      # https://www.kraxel.org/blog/2021/05/qemu-cut-paste/
-      "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
-      "-device virtio-serial-pci"
-      "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
-    ];
-  };
-
-  # A default user able to use sudo
   users.users.guest = {
     isNormalUser = true;
     home = "/home/guest";
@@ -35,35 +17,9 @@
     initialPassword = "guest";
   };
 
+  services.getty.autologinUser = "test";
   security.sudo.wheelNeedsPassword = false;
 
-  # X configuration
-  services.xserver.enable = true;
-  services.xserver.layout = "fr";
-
-  services.xserver.displayManager.autoLogin.user = "guest";
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.desktopManager.xfce.enableScreensaver = false;
-
-  services.xserver.videoDrivers = [ "qxl" ];
-
-  # For copy/paste to work
-  services.spice-vdagentd.enable = true;
-
-  # Enable ssh
-  services.sshd.enable = true;
-
-  # Included packages here
-  nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    dig
-    hey
-    httpie
-    google-chrome
-    neovim
-    wget
-    wrk
-  ];
-
-  system.stateVersion = "22.11";
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
 }
